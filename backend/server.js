e3bc295e288b7319a8cb593861bd9aa3e31b27d1
vamshi-2705 <vamshikrishna7036@gmail.com>
2026-08-17 19:56:43 +0530

    feat: connect PostgreSQL database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 require("dotenv").config();
 
+const pool = require("./config/db");
+
 const app = express();
 
 app.use(express.json());
@@ -9,6 +11,23 @@ app.get("/", (req, res) => {
     res.json({
         message: "JWT Authentication API is running"
     });
+});
+
+app.get("/db-test", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW()");
+
+        res.json({
+            message: "Database connected successfully",
+            time: result.rows[0].now
+        });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Database connection failed"
+        });
+    }
 });
 
 const PORT = process.env.PORT || 5000;
